712073cebd3401b43675112c64c02070f47ddda9
sean <seanowenhayes@gmail.com>
2015-12-06 09:55:36 +0100

    Added headers, particularly user-agent to make it harder to detect the crawler.

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 var request = require('request');
+
+var HEADERS = {
+    'accept': "application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5"
+    ,
+    'accept-language': 'en-US,en;q=0.8'
+    ,
+    'accept-charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3'
+    ,
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.130 Safari/537.36'
+};
+
+
 module.exports = function (page, spider, next) {
-    request(page.url, function (err, response, body) {
+
+    request({
+        uri: page.url,
+        headers: HEADERS,
+        method: 'GET'
+    }, function (err, response, body) {
         page.setContent(body);
         page.response = response;
         next();
