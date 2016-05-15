27b5af41b405cafbddf744ef20c3f1dc19505412
VERMICULITE <seanowenhayes@gmail.com>
2016-05-15 07:49:48 +0000

    Seperated out the request logic to make the module more useful.

--- index.js
+++ index.js
@@ -1,37 +1,18 @@
 "use strict";
 
 let through2 = require('through2');
-var request = require('request');
-
-var HEADERS = {
-    'accept': "application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5"
-    ,
-    'accept-language': 'en-US,en;q=0.8'
-    ,
-    'accept-charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3'
-    ,
-    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.130 Safari/537.36'
-};
-
+var request = require('./lib/request');
 
 module.exports = function(options) {
 
-    options = options || {};
-    var headers = options.headers || HEADERS;
-    var encoding = options.encoding || 'utf8';
+    let requester = request(options);
 
     return through2.obj(function (page, enc, next) {
-
-        request({
-            uri: page.url,
-            headers: headers,
-            method: 'GET',
-            encoding: encoding
-        }, (err, response, body) => {
+        requester(page.url, (err, response, body) => {
             page.content = body;
             page.response = response;
             this.push(page);
             next();
-        });
+        })
     })
 };
