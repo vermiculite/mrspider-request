31b8f7b1902bdce4cc4b591fa26a3163a7538705
seanowenhayes <seanowenhayes@gmail.com>
2018-05-01 21:16:10 +0200

    Updated to be general purpose crawling get function.

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-"use strict";
+const request = require('request')
+const headers = {
+  'accept': 'application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5',
+  'accept-language': 'en-US,en;q=0.8',
+  'accept-charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3'
+}
 
-let through2 = require('through2');
-var request = require('./lib/request');
+const firefox = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_4; en-US) ' +
+  'AppleWebKit/534.7 (KHTML, like Gecko) Chrome/7.0.517.41 Safari/534.7'
 
-module.exports = function(options) {
+function doRequest ({userAgent = firefox}) {
 
-    let requester = request(options);
+  return function (url, callback) {
+    request.get({
+      url,
+      headers: {
+        ...headers,
+        userAgent,
+      }
+    }, callback)
+  }
+}
 
-    return through2.obj(function (page, enc, next) {
-        requester(page.url, (err, response, body) => {
-            page.content = body;
-            page.response = response;
-            this.push(page);
-            next();
-        })
-    })
-};
+module.exports = function(options = {}) {
+  return doRequest(options)
+}
 
-module.exports.request = request;
--- test/index.js
+++ test/index.js
@@ -1,25 +1,66 @@
-var mrspiderRequest = require('..')();
-var nock = require('nock');
-var sinon = require('sinon');
-var chai = require('chai');
-var should = chai.should();
+const MrspiderRequest = require('..')
+const nock = require('nock')
+const sinon = require('sinon')
+const chai = require('chai')
+const should = chai.should()
 
-describe('mrspider-request', function(done) {
+describe('mrspider-request', function () {
 
-    it('should work', function() {
-        var address = 'http://www.abc.com';
-        var html = '<h1>HI there!</h1>';
-        var abc = nock(address)
-            .get('/')
-            .reply(200, html);
-        var page = {
-            url: address,
-            setContent: sinon.spy()
-        };
-        mrspiderRequest._transform(page, 'utf8', function() {
-            page.setContent.calledOnce.should.equal(true);
-            page.setContent.firstCall.args[0].should.equal(html);
-            done();
-        });
-    });
-});
+  it('should work', function (done) {
+    const mrspiderRequest = MrspiderRequest()
+    const address = 'http://www.abc.com'
+    const html = '<h1>HI there!</h1>'
+    const abc = nock(address)
+      .get('/')
+      .reply(200, html)
+
+    mrspiderRequest(address, function (err, res, body) {
+      body.should.equal(html)
+      should.not.exist(err)
+      should.exist(res)
+      done()
+    })
+  })
+
+  it('should use the default useragent if none supplied', function(done) {
+    const mrspiderRequest = MrspiderRequest()
+    const address = 'http://www.abc.com'
+    const html = '<h1>HI there!</h1>'
+
+    const abc = nock(address, {
+      reqheaders: {
+        'userAgent': 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_4; en-US) ' +
+        'AppleWebKit/534.7 (KHTML, like Gecko) Chrome/7.0.517.41 Safari/534.7'
+      }
+    })
+      .get('/')
+      .reply(200, html)
+
+    mrspiderRequest(address, function (err, res, body) {
+      should.not.exist(err)
+      should.exist(res)
+      body.should.equal(html)
+      done()
+    })
+  })
+
+  it('should use the useragent if supplied', function(done) {
+    const mrspiderRequest = MrspiderRequest({userAgent: 'Banana'})
+    const address = 'http://www.abc.com'
+    const html = '<h1>HI there!</h1>'
+    const abc = nock(address, {
+      reqheaders: {
+        'userAgent': 'Banana'
+      }
+    })
+      .get('/')
+      .reply(200, html)
+
+    mrspiderRequest(address, function (err, res, body) {
+      should.not.exist(err)
+      should.exist(res)
+      body.should.equal(html)
+      done()
+    })
+  })
+})
